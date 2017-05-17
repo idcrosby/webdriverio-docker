68afc1b42da2ce3b9a0099d2461b17a2518c97e3
Ian Crosby <ian.daniel.crosby@gmail.com>
2017-05-17 16:36:54 +0400

    update test

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,6 +4,6 @@ describe('webdriver.io page', function() {
   it('should have the right title', function () {
     browser.url('http://webdriver.io');
     var title = browser.getTitle();
-    assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js');
+    assert.equal(title, 'Profile Watcher');
   });
 });
